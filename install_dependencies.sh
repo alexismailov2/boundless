f0ea1ea31e7631d2175ed30b2c805b902df062bd
always_learning <61051193+codenlighten@users.noreply.github.com>
2023-11-03 01:17:15 -0400

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -9,7 +9,7 @@ sudo apt-get update -y
 sudo apt-get install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3 
 libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev 
 libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libboost-all-dev 
-software-properties-common git libminiupnpc-dev libzmq3-dev libqt4-dev wget unzip
+software-properties-common git libminiupnpc-dev libzmq3-dev libqt4-dev wget unzip qt4-qmake && sudo apt-get install git && sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils && sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libboost-all-dev software-properties-common  libminiupnpc-dev && sudo apt-get install libzmq3-dev && sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler && sudo apt-get install libqt4-dev libprotobuf-dev protobuf-compiler
 
 # Download libdb4.8 sources
 wget http://download.oracle.com/berkeley-db/db-4.8.30.zip
