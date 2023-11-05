5dbcf6285f3c067753d848979e8957a8dd6bee6a
always_learning <61051193+codenlighten@users.noreply.github.com>
2023-11-04 20:24:10 -0400

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y apt-utils software-properties-common
 # Install required packages
 sudo add-apt-repository -y ppa:ubuntuhandbook1/ppa
 sudo apt-get update -y
-sudo apt-get install -y git build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3 libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libboost-all-dev software-properties-common libminiupnpc-dev libzmq3-dev libqt4-dev wget unzip qt4-qmake
+sudo apt-get install -y git build-essential libtool libdb++-dev autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3 libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libboost-all-dev software-properties-common libminiupnpc-dev libzmq3-dev libqt4-dev wget unzip qt4-qmake
 
 # Download libdb4.8 sources
 wget http://download.oracle.com/berkeley-db/db-4.8.30.zip
