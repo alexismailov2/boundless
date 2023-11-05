670a211023e14fcd8bbd32b83d5413fd80586134
always_learning <61051193+codenlighten@users.noreply.github.com>
2023-11-05 00:19:01 -0400

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1177,6 +1177,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
+//{"167.172.129.199","167.172.129.199"},
  {NULL, NULL}
 };
 
